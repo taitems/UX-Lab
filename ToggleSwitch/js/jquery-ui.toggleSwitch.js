093cd3dad31952e4a455c04bf5a955914e8455ee
corinis <niko.berger@corinis.com>
2013-03-07 13:43:16 +0100

    allow disable of control using <select disable> attribute

--- ToggleSwitch/js/jquery-ui.toggleSwitch.js
+++ ToggleSwitch/js/jquery-ui.toggleSwitch.js
@@ -21,6 +21,10 @@ jQuery.fn.toggleSwitch = function (params) {
         $(selectObj).find("option").each(function (i, item) {
             $contain.append("<label>" + $(item).text() + "</label>");
         }).end().addClass("ui-toggle-switch");
+        
+        $(selectObj).hide();
+		// add pointer to the original object
+		$contain.data("select", selectObj);
 
         // generate slider with established options
         var $slider = $("<div />").slider({
@@ -32,7 +36,7 @@ jQuery.fn.toggleSwitch = function (params) {
                 var roundedVal = Math.round(ui.value / 100);
                 var self = this;
                 window.setTimeout(function () {
-                    toggleValue(self.parentNode, roundedVal);
+                    toggleValue(self.parentNode, roundedVal, false);
                 }, 11);
             },
             range: (options.highlight && !$(selectObj).data("hideHighlight")) ? "max" : null
@@ -41,34 +45,53 @@ jQuery.fn.toggleSwitch = function (params) {
         // put slider in the middle
         $slider.insertAfter(
             $contain.children().eq(0)
-		    );
+		);
 
         // bind interaction
         $contain.delegate("label", "click", function () {
             if ($(this).hasClass("ui-state-active")) {
                 return;
             }
+            // prevent change for disabled objects
+        	if($(selectObj).is(":disabled")) {
+        		return;
+        	}
+        	
             var labelIndex = ($(this).is(":first-child")) ? 0 : 1;
-            toggleValue(this.parentNode, labelIndex);
+            toggleValue(this.parentNode, labelIndex, false);
         });
 
-        function toggleValue(slideContain, index) {
-            var $slideContain = $(slideContain), $parent = $slideContain.parent();
+        /**
+         * @param slideContain {object} the slide container
+         * @param index {number} the index to set
+         * @param ignoreEqual {boolean} true if this is the initialization call (avoids qual check) 
+         */
+        function toggleValue(slideContain, index, ignoreEqual) {
+            var $slideContain = $(slideContain), $parent = $($slideContain.data("select"));
             /* if this option was already selected, then do nothing */
-            if ($parent.find("option").eq(index).attr("selected")) {
+            if (!ignoreEqual && $parent.find("option").eq(index).attr("selected")) {
                 return;
             }
             $slideContain.find("label").eq(index).addClass("ui-state-active").siblings("label").removeClass("ui-state-active");
             $parent.find("option").attr("selected", false).eq(index).attr("selected", true);
-            $parent.find("select").trigger("change");
+            $parent.trigger("change");
+            $parent.removeData("toggleValueChange");
             $slideContain.find(".ui-slider").slider("value", index * 100);
         }
+        // check for disabled
+    	if($(selectObj).is(":disabled")) {
+    		$contain.find("label").addClass("ui-state-disabled");
+    		$slider.addClass("ui-state-disabled");
+    		$slider.slider( "option", "disabled", true );
+    	}
+
 
         // initialise selected option
-        $contain.find("label").eq(selectObj.selectedIndex).click();
+        var initialSelection = $contain.find("label").eq(selectObj.selectedIndex);
+        toggleValue($contain, ($(initialSelection).is(":first-child")) ? 0 : 1, true);
 
         // add to DOM
-        $(selectObj).parent().append($contain);
+        $(selectObj).before($contain);
 
     }
 };
